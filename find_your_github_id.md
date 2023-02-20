c1bd69563844f2b6d1c41d4c53fc83156337069e
Fabrice <neo.oseis@gmail.com>
2023-02-19 22:24:27 -0500

    Add (#2)
    * added main file
    
    * Update main.md
    
    * Update main.md
    
    * Update main.md
    
    * Update main.md
    
    * Update main.md
    
    * Update main.md
    
    * Update main.md
    
    * Update main.md
    
    * Update main.md
    
    * Update main.md
    
    * adding 2 more sec guides
    
    * Update remove_pushed_credentials.md
    
    * Update secure_code_scan.md
    
    * Create find_your_github_id.md
    
    * Update find_your_github_id.md
    
    * Update secure_code_scan.md
    
    * add spell-check
    
    * testing aspell
    
    * testing aspell
    
    * testing aspell
    
    * testing aspell
    
    * testing aspell
    
    * testing aspell
    
    * testing aspell
    
    * testing aspell
    
    * empty
    
    * fixed the script
    
    * fixed the script
    
    * fixed the script

---
+++ find_your_github_id.md
@@ -0,0 +1,1 @@
+To fins your github ID, go to a browser and enter https://api.github.com/users/<your_gihub_user_name>.
---
+++ spell_check.py
@@ -0,0 +1,24 @@
+# from spellchecker import SpellChecker
+#
+# spell = SpellChecker()
+#
+# # find those words that may be misspelled
+# misspelled = spell.unknown(["cmputr", "watr", "study", "wrte"])
+# # misspelled = spell.unknown('words.txt')
+#
+# for word in misspelled:
+# 	# Get the one `most likely` answer
+# 	print(spell.correction(word))
+#
+# 	# Get a list of `likely` options
+# 	print(spell.candidates(word))
+
+import language_tool_python
+tool = language_tool_python.LanguageTool('en-US')
+
+text = "word.txt"
+
+# get the matches
+matches = tool.check(text)
+
+matches
